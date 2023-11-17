556de297be07c3cebe40ff8146330ab7e649ae3d
MarcPL <marclert@gmail.com>
2023-11-18 06:08:02 +0700

    adding notes

--- test.js
+++ test.js
@@ -90,4 +90,72 @@
 //     i = i + 1;
 // }
 
-// Nested Loops
+// Exercise: Working with conditionals and loops
+// for (var i = 1; i < 11; i++) {
+//     if (i == 1) {
+//         console.log("Gold Medal")
+//     }
+//     else if (i == 2) {
+//         console.log("Silver Medal")
+//     }
+//     else if (i == 3) {
+//         console.log("Bronze Medal")
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     switch (i) {
+//         case 1:
+//             console.log("Gold Medal")
+//             break
+//         case 2:
+//             console.log("Silver Medal")
+//             break
+//         case 3:
+//             console.log("Bronze Medal")
+//             break
+//         default:
+//             console.log(i)        
+//     }
+// }
+
+/*
+* coimment
+*/
+
+// Functions
+// function listArrayItems(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         console.log(arr[i]) //display the array item where the index is euqal to i
+//     }
+// }
+
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+// listArrayItems(colors); //display all items in the array at once
+
+// Functions with a nested condional statement
+// function listArrayItems(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] == 'red') {
+//             console.log(i*100, "tomato!")
+//         } else {
+//             console.log(i*100, arr[i])
+//         }
+//     }
+// }
+
+function letterFinder(word, match) {
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] == match) {
+            console.log("Found the " + match + " at " + i)
+        }
+        else {
+            console.log("---No match found at" + i)
+        }
+    }
+}
+
+letterFinder("test", "t")
